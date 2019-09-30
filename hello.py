4bd1143cc7caefe631c83ac4ec19899959f8f662
XcaliberRage <XcaliberRage@users.noreply.github.com>
2019-09-30 15:42:17 +0000

    automated commit by submit50

--- hello.py
+++
@@ -1,8 +0,0 @@
-from cs50 import get_string
-
-while True:
-    s = get_string("What is your name? ")
-    if s != "":
-        break
-
-print("Hello, ", s)
--- mario.py
+++ mario.py
@@ -1,5 +1,6 @@
 import cs50
 
+
 def main():
     # get input
     while True:
@@ -11,14 +12,17 @@ def main():
     # Use the input to print the pyramid
     printPyramid(height)
 
+
 def printPyramid(thisHeight):
 
     # get each row (counting 1 higher)
     for level in range(thisHeight):
         row = level + 1
-        #Level defines how many blocks will appear in a row half
+        # Level defines how many blocks will appear in a row half
         printRow(row, thisHeight)
 
+
+# Print the content of this row
 def printRow(thisRow, maxWidth):
 
     # Print spaces equal to height - row
@@ -32,10 +36,13 @@ def printRow(thisRow, maxWidth):
     # Print the blocks again but new line after
     printBlocks(thisRow, "\n")
 
+# Print blocks required for this half of the pyramid
 def printBlocks(blockCount, ending):
     for block in range(blockCount):
         print("#", end="")
     print("", end=ending)
 
+
+# Execute the main line
 if __name__ == "__main__":
     main()
